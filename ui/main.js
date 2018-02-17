505cfe905d3dc572d25ed32073bc57214833d60f
amitagarwalaa57 <36447666+amitagarwalaa57@users.noreply.github.com>
2018-02-17 20:00:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ element.innerHTML = 'New value';
 //Move the image
 var img = document.getElementById('madi');
 img.onclick = function(){
-  madi.style.marginLeft='100 px';  
+  img.style.marginLeft='100px';  
 };
