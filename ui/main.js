0ca571ff140f42cc0d94e3f0e48f040f3c3df3ac
amitagarwalaa57 <36447666+amitagarwalaa57@users.noreply.github.com>
2018-02-18 14:50:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,7 +46,8 @@ submit.onclick = function(){
             if(request.status === 200){
                 //capture a list of names and render it as list
     
-    var names = ['name1','name2','name3'];
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list = '';
     for(var i=0; i<names.length ; i++){
         list += '<li>' + names[i] + '</li>';
