75d9b65a80e069c7be077d2e6e914ad48977d731
amitagarwalaa57 <36447666+amitagarwalaa57@users.noreply.github.com>
2018-02-25 18:23:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     database: 'amitagarwalaa57',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD,
+    password: db-amitagarwalaa57-81883
 };
 
 
