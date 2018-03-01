b462dde133ca07ffa0c3be1e8e133d781f2b35c1
Amit Kumar Agarwal <36447666+amitagarwalaa57@users.noreply.github.com>
2018-03-01 23:03:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
-    cookie: { maxAge: 1000*60*60*24*30}
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
 
 app.get('/', function (req, res) {
