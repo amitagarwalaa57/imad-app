ab6fca8601ea42276cb7dc53ed35cc24d56e0c53
Amit Kumar Agarwal <36447666+amitagarwalaa57@users.noreply.github.com>
2018-03-01 23:22:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,8 @@ app.post('/login', function (req, res) {
 });
 
 app.get('/check-login', function (req, res) {
-    if(req.session && req.session.auth && req.session.auth.userId){
+    if(req.session && req.session.auth && req.session.auth.userId)
+    {
         req.send('You are logged in: ' + req.session.auth.userId.toString());
     }
     else{
