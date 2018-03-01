6e898dc053f8c876c042f44b75b1c060def4c382
Amit Kumar Agarwal <36447666+amitagarwalaa57@users.noreply.github.com>
2018-03-01 18:39:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var config = {
     user: 'amitagarwalaa57',
     database: 'amitagarwalaa57',
@@ -17,6 +18,20 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+function hash (input,salt) {
+    //How to create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input,'this-is-some-random-string');
+   res.send(hashedString);
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function (req, res){
     //make a select response
