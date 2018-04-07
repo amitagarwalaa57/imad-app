e2787b3bd6e3f527a552a05025757427ecaebc5c
Amit Kumar Agarwal <36447666+amitagarwalaa57@users.noreply.github.com>
2018-04-07 19:18:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -429,7 +429,7 @@ app.get('/articles/:articleName', function (req, res) {
 
             var articleData = result.rows[0];
 
-            res.send(createTemplate(articleData))
+            res.send(createTemplate(articleData));
 
         }
 
